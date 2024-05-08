eeb44874c9bbabf39ce43e7e3b58ec770ad1d579
James Vu <jamesvu1998@gmail.com>
2024-05-09 02:49:11 +1000

    update README and implement multi-ripples

--- app/data.ts
+++ app/data.ts
@@ -11,7 +11,9 @@ export const work_exp: Record<
   ATO: {
     duration: "FEB 2021 - MAR 2023",
     role: "Data Engineer",
-    points: ["test"],
+    points: [
+      "Automated testing of new cloud infrastructure deployments using Python to ensure that they are configured correctly as per requirements. Test suite covered resources that were provisioned from a variety of AWS services including EC2, S3, IAM, Route53, EFS, and KMS.",
+    ],
     link: "https://ato.gov.au",
   },
 };
--- app/routes/_index/components/ripple.tsx
+++ app/routes/_index/components/ripple.tsx
@@ -1,10 +1,4 @@
-import {
-  Dispatch,
-  MouseEvent,
-  SetStateAction,
-  useEffect,
-  useState,
-} from "react";
+import { Dispatch, MouseEvent, SetStateAction, useRef, useState } from "react";
 
 export function RippleButton({
   text,
@@ -15,9 +9,12 @@ export function RippleButton({
   activeStint: string;
   activeStintSetter: Dispatch<SetStateAction<string>>;
 }) {
-  const [coords, setCoords] = useState({ x: -1, y: -1 });
-  const [rippleCount, setRippleCount] = useState(0);
-  const [isRippling, setIsRippling] = useState(false);
+  const [coords, setCoords] = useState<{ x: number; y: number }>({
+    x: -1,
+    y: -1,
+  });
+  const [rippleCount, setRippleCount] = useState<number>(0);
+  const rippleArray = useRef<number[]>([]);
 
   function triggerRipple(e: MouseEvent) {
     const element = e.currentTarget as HTMLElement;
@@ -27,20 +24,10 @@ export function RippleButton({
       y: e.clientY - bounding_rect.y,
     });
     setRippleCount(rippleCount + 1);
+    rippleArray.current.push(rippleCount + 1);
+    setTimeout(() => rippleArray.current.shift(), 300);
     activeStintSetter(text);
   }
-
-  useEffect(() => {
-    let timeout_id: ReturnType<typeof setTimeout>;
-    if (coords.x !== -1 && coords.y !== -1) {
-      setIsRippling(true);
-      timeout_id = setTimeout(() => setIsRippling(false), 300);
-    }
-
-    return () => {
-      clearTimeout(timeout_id);
-    };
-  }, [rippleCount, coords]);
 
   return (
     <button
@@ -48,18 +35,16 @@ export function RippleButton({
       className={`relative h-12 w-full overflow-hidden px-4 py-3 text-left text-slate-400 active:text-teal-400 ${activeStint === text ? "bg-cyan-400/10 text-teal-400" : ""}`}
       onClick={triggerRipple}
     >
-      {isRippling ? (
+      {rippleArray.current.map((num) => (
         <span
-          key={rippleCount}
+          key={num}
           className={`absolute block h-1 w-1 animate-ripple rounded-full bg-teal-400 `}
           style={{
             top: coords.y,
             left: coords.x,
           }}
         ></span>
-      ) : (
-        ""
-      )}
+      ))}
       <span className="relative z-10 font-mono">{text.toUpperCase()}</span>
     </button>
   );
